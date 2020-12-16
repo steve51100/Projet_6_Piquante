cc70a049e523e4212df69fd3e0c6f63e417776b1
Steve51100 <sttaz51@hotmail.fr>
2020-12-16 16:23:53 +0100

    ajout de fonction  pour cree un article = pb 400 a corriger

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose');
 
 
 
-//import  du router
+//import  des routes
 const userRoutes = require('./routes/User');
-
+const Sauce = require('./models/Sauce');
 
 //acces a la base de donner mongoose
 mongoose.connect('mongodb+srv://steve:Maison51@cluster0.jpewg.mongodb.net/Piquante?retryWrites=true&w=majority',
@@ -29,6 +29,42 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+//creation des produits sauces
+
+app.post('/api/sauces', (req, res, next) => {
+  delete req.body._id;
+  const sauce = new Sauce({
+    ...req.body
+  });
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.use('/api/sauces', (req, res, next) => {
+  const sauce = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(sauce);
+});
+
+
+
 
 // Enregistrement des routeurs
 app.use('/api/auth', userRoutes);
