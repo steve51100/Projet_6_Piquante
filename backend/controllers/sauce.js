9ef75cbca3466730dcfc796bce1a677795c58929
Steve51100 <sttaz51@hotmail.fr>
2020-12-18 09:09:18 +0100

    ajout de  la fonction modifier (controlleur + route)

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,32 +2,47 @@ const Sauce = require('../models/Sauce');
 
 
 exports.getAllSauces = (req, res, next) => {
-    Sauce.find()
-      .then(sauces => res.status(200).json(sauces))
+  Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+};
+
+// Lecture d'une sauce avec son ID (Get/:id)
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+};
+
+// Création d'une nouvelle sauce (Post)
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+
+  // Création d'un nouvel objet Sauce
+  const sauce = new Sauce({
+    ...sauceObject,
+    // Création de l'URL de l'image : http://localhost:3000/image/nomdufichier 
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  // Enregistrement de l'objet sauce dans la base de données
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+
+// Modification d'une sauce
+exports.modifySauce = (req, res, next) => {
+  const sauceObject = req.file ? {
+
+    //il exist deja une image
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+   //Si il n'y a pas d'image
+   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
-  };
-  
-  // Lecture d'une sauce avec son ID (Get/:id)
-  exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(404).json({ error }));
-  };
-  
-  // Création d'une nouvelle sauce (Post)
-  exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
-  
-    // Création d'un nouvel objet Sauce
-    const sauce = new Sauce({
-      ...sauceObject,
-      // Création de l'URL de l'image : http://localhost:3000/image/nomdufichier 
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-    // Enregistrement de l'objet sauce dans la base de données
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  };
-  
+};
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -10,7 +10,7 @@ const multer = require('../middleware/multer-config');
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
-
+router.put('/:id', auth,multer,sauceCtrl.modifySauce);
 
 
 module.exports = router;
