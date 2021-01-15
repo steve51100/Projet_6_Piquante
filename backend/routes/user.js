6067cc7b8e1e41c49b9c52965ba9570de5be9b39
Steve51100 <sttaz51@hotmail.fr>
2021-01-15 10:12:04 +0100

    ajout module bloquant attaque de force

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const verifyPassword = require('../middleware/pass');
 
+//module blocant les attaque de force utilisateur
+const rateLimite = require('express-rate-limit');
+//Limite la connection abusive
+const limiter = rateLimite({
+    windowMs: 3 * 60 * 1000, // 3 minutes
+  max: 3, // limit each IP to 3 requests per windowMs
+  message: "trop de requete abusive , vous devez attendre 3 minutes",
+  
+});
+console.log(limiter);
 // Routes
 router.post('/signup',verifyPassword, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login',limiter, userCtrl.login);
 
 module.exports = router;
